a8575e382179333801118b4dd3e254aca60aa22d
user <user@PC32>
2024-11-07 14:43:40 -0500

    Laura3

--- src/fichas_medicas/Fichas_medicas.java
+++ src/fichas_medicas/Fichas_medicas.java
@@ -12,6 +12,7 @@ public class Fichas_medicas {
      /**
      * Primer cambio 6 noviembre 2023
      * Prueba Jose 7/11/2024 14:28pm
+     * Prueba Laura 7/11/2024 14:43pm
      */
     public static void main(String[] args) {
         System.out.println("Practicas Bernardino");  // Imprime un saludo
